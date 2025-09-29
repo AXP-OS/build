38747e29f816fb1ceedc332a41f9624683d3f806
steadfasterX <steadfasterX@users.noreply.github.com>
2025-09-29 12:15:32 +0200

    cve: excludes for FP4
    Signed-off-by: steadfasterX <steadfasterX@users.noreply.github.com>

--- Scripts/Common/Fix_CVE_Patchers.sh
+++ Scripts/Common/Fix_CVE_Patchers.sh
@@ -143,7 +143,8 @@ commentPatches android_kernel_fairphone_sm7225.sh \
     "CVE-2025-21859/ANY/0001.patch" \
     "CVE-2025-38034" \
     "CVE-2022-49980" \
-    "CVE-2022-49988/ANY/0001.patch"
+    "CVE-2022-49988/ANY/0001.patch" \
+    "CVE-2025-38677/ANY/0001.patch" "CVE-2025-38099/ANY/0001.patch" 
 
 commentPatches android_kernel_fxtec_msm8998.sh \
     "0008-Graphene-Kernel_Hardening-slab" \
